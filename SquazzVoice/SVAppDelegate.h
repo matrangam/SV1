e471fc4e1a6b493a7bc47bf10a2272dcfec30287
Michael Matranga <mike@dmgctrl.com>
2012-08-18 13:56:00 -0400

    added random path generation, static audioplayer method for now.

--- SquazzVoice/SVAppDelegate.h
+++ SquazzVoice/SVAppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  SVAppDelegate.h
-//  SquazzVoice
-//
-//  Created by Michael Matranga on 8/18/12.
-//  Copyright (c) 2012 Michael Matranga. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SVAppDelegate : UIResponder <UIApplicationDelegate>
--- SquazzVoice/SVViewController.m
+++ SquazzVoice/SVViewController.m
@@ -1,11 +1,43 @@
 #import "SVViewController.h"
+#import "stdlib.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface SVViewController ()
 
 @end
 
-@implementation SVViewController
+@implementation SVViewController {
+    AVAudioPlayer* _audioPlayer;
+}
 
++ (NSString*) randomFilePath
+{
+    NSDictionary* dict = @{
+        @0 : @"ONE",
+        @1 : @"TWO",
+        @2 : @"THREE",
+        @3 : @"FOUR",
+        @4 : @"FIVE",
+        @5 : @"SIX",
+        @6 : @"SEVEN",
+        @7 : @"EIGHT",
+        @8 : @"NINE",
+        @9 : @"TEN",
+        @10 : @"ELEVEN",
+        @11 : @"TWELVE",
+    };
+    
+    NSArray* allKeys = [dict allKeys];
+    NSInteger randomIteger = arc4random_uniform([allKeys count]);
+    NSNumber* randNum = [NSNumber numberWithInteger:randomIteger];
+    return [dict objectForKey:randNum];
+}
+
++ (AVAudioPlayer*) audioPlayerWithURL:(NSURL*)URL
+{
+    NSError* error = nil;
+    return [[AVAudioPlayer alloc] initWithContentsOfURL:URL error:&error];
+}
 
 - (void) viewDidUnload
 {
@@ -15,6 +47,7 @@
 
 - (IBAction) voiceButtonPressed:(id)sender
 {
-    
+    _audioPlayer = [SVViewController audioPlayerWithURL:[NSURL URLWithString:[SVViewController randomFilePath]]];
+    NSLog(@"%@", [SVViewController randomFilePath]);
 }
 @end
